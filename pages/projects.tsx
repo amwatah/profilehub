c3c560f3aec18d8d3995165ad72fd89eefaf4b9d
Amwatah Obuya <jackalltrade2020@gmail.com>
2022-12-19 08:53:51 +0300

    new projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -9,8 +9,23 @@ const ProjectsPage = () => {
       <h3>MY PROJECTS</h3>
       <section className="projects grid grid-cols-1 gap-1 sm:grid-cols-2 md:grid-cols-3 mx-[2%]">
         <ProjectCard
+          projectName="Planbee"
+          desribtion="A event planner"
+          projectImage={
+            <Image
+              src="/images/planbee.png"
+              className=" w-full h-full"
+              layout="fill"
+            />
+          }
+          website="https://planbee-amwatah.vercel.app/"
+          github="https://github.com/amwatah/planbee"
+          tags={["Next Js", "Mongodb", "Typescript", "Trpc", "Prisma"]}
+        />
+
+        <ProjectCard
           projectName="CALCULATOR"
-          desribtion="A Simple"
+          desribtion="Everybody once made a calc"
           projectImage={
             <Icon
               icon="flat-color-icons:calculator"
@@ -48,6 +63,20 @@ const ProjectsPage = () => {
           website="https://amwatah-todo.vercel.app/"
           github="https://github.com/amwatah/todo-app"
           tags={["Beginner", "Next JS", "Typescript"]}
+        />
+        <ProjectCard
+          projectName="Planbee"
+          desribtion="A event planner"
+          projectImage={
+            <Image
+              src="/images/contacts.png"
+              className=" w-full h-full"
+              layout="fill"
+            />
+          }
+          website="https://amwatah-contacts.vercel.app/"
+          github="https://github.com/amwatah/numbers"
+          tags={["Next Js", "Mongodb", "Typescript", "Trpc", "Prisma"]}
         />
       </section>
     </div>
