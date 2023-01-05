d1ee0778a19dd04106b35adcf7f56b2dae1f463b
Amwatah Obuya <jackalltrade2020@gmail.com>
2023-01-05 11:28:27 +0300

    new project

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -9,12 +9,42 @@ const ProjectsPage = () => {
       <h3>MY PROJECTS</h3>
       <section className="projects grid grid-cols-1 gap-1 sm:grid-cols-2 md:grid-cols-3 mx-[2%]">
         <ProjectCard
+          projectName="Electrika"
+          desribtion="An online electronics shop cart"
+          projectImage={
+            <Image
+              src="/images/electrika.png"
+              className=" w-full h-full object-cover"
+              layout="fill"
+              alt="project"
+            />
+          }
+          website="https://planbee-amwatah.vercel.app/"
+          github="https://github.com/amwatah/elecrika"
+          tags={["Next Js", "Mongodb", "Typescript", "Trpc", "Prisma"]}
+        />
+        <ProjectCard
+          projectName="TIC-TAC-TOE GAME"
+          desribtion="A simple board game"
+          projectImage={
+            <Image
+              src="/images/tic-tac-toe.png"
+              className=" w-full h-full"
+              layout="fill"
+              alt="project"
+            />
+          }
+          website="https://amwatah-tic-tac-toe.vercel.app/"
+          github="https://github.com/amwatah/tic-tac-toe"
+          tags={["Beginner", "Next JS", "Mantine", "Typescript"]}
+        />
+        <ProjectCard
           projectName="Planbee"
           desribtion="A event planner"
           projectImage={
             <Image
               src="/images/planbee.png"
-              className=" w-full h-full"
+              className=" w-full h-full object-cover"
               layout="fill"
               alt="project"
             />
@@ -37,28 +67,14 @@ const ProjectsPage = () => {
           github="https://github.com/amwatah/next-calculator-pwa"
           tags={["React", "PWA", "Typescript"]}
         />
+
         <ProjectCard
-          projectName="TIC-TAC-TOE GAME"
-          desribtion="A simple board game"
-          projectImage={
-            <Image
-              src="/images/tic-tac-toe.png"
-              className=" w-full h-full"
-              layout="fill"
-              alt="project"
-            />
-          }
-          website="https://amwatah-tic-tac-toe.vercel.app/"
-          github="https://github.com/amwatah/tic-tac-toe"
-          tags={["Beginner", "Next JS", "Mantine", "Typescript"]}
-        />
-        <ProjectCard
-          projectName="TODO APP"
+          projectName="TASKER"
           desribtion="A  way to track and manage daily tasks"
           projectImage={
             <Image
               src="/images/todo.png"
-              className=" w-full h-full"
+              className=" w-full h-full object-cover"
               layout="fill"
               alt="project"
             />
@@ -68,12 +84,12 @@ const ProjectsPage = () => {
           tags={["Beginner", "Next JS", "Typescript"]}
         />
         <ProjectCard
-          projectName="Planbee"
-          desribtion="A event planner"
+          projectName="Contact Manager"
+          desribtion="Create and delete  contacts"
           projectImage={
             <Image
               src="/images/contacts.png"
-              className=" w-full h-full"
+              className=" w-full h-full object-cover"
               layout="fill"
               alt="project"
             />
